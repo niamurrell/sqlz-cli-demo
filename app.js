f7e69c46c114af81951b0c682454d3508c46e852
niamurrell <niamurrell@users.noreply.github.com>
2019-02-07 23:03:59 +0000

    Add Create routes & controllers

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Require our routes into the application.
+require('./routes')(app);
+app.get('*', (req, res) => res.status(200).send({
+	message: 'Welcome to the beginning of nothingness.',
+}));
+
 // Setup a default catch-all route that sends back a welcome message in JSON format.
 app.get('*', (req, res) => res.status(200).send({
 	message: 'Welcome to the beginning of nothingness.',
